22316fa219c1f77158c379f9d5317423bb3ec9b2
Dominick Madden <dominick.madden@gmail.com>
2017-10-24 11:03:16 +0200

    Refactor constants

---
+++ src/frontend/constants/URLs.js
@@ -0,0 +1,4 @@
+export const URLs = {
+  FETCH_NODE_IDS_URL: 'http://localhost:9999/boxes/ids',
+  FETCH_NODE_URL: 'http://localhost:9999/boxes/'
+};
---
+++ src/frontend/constants/index.js
@@ -0,0 +1,11 @@
+import Actions from './Actions';
+import ReactDnDConstants from './ReactDnDConstants';
+import ReactToolboxVariables from './ReactToolboxVariables';
+import URLs from './URLs';
+
+export {
+  Actions,
+  ReactDnDConstants,
+  ReactToolboxVariables,
+  URLs
+};
