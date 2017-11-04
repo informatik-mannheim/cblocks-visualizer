ff2d7d904a2579f26d568afbac1cd90cf432ed3a
Dominick Madden <dominick.madden@gmail.com>
2017-11-04 21:26:21 +0100

    Add dev-env startup script

---
+++ start_env.ps1
@@ -0,0 +1,31 @@
+$mongo_dir = 'C:\Program Files\MongoDB\Server\3.4\bin'
+$registry_dir = '~\Documents\GitHub\registry'
+$cblocks_dir = '~\Documents\GitHub\cblocks'
+
+$MongoBlock = {
+  Set-Location $args[0]
+  .\mongod.exe
+}
+
+$RegistryBlock = {
+  Set-Location $args[0]
+  npm start
+}
+
+$CBlocksBlock = {
+  Set-Location $args[0]
+  npm start
+}
+
+Get-Job|Stop-Job
+Get-Job|Remove-Job
+
+$j1 = Start-Job $MongoBlock -Name "MongoDBJob"-ArgumentList $mongo_dir
+$j2 = Start-Job $RegistryBlock -Name "cBlocksRegistryJob" -ArgumentList $registry_dir
+$j3 = Start-Job $CBlocksBlock -Name "cBlocksUIJob" -ArgumentList $cblocks_dir
+
+Get-Job
+Wait-Job -Id $j1.id,$j2.id,$j3.id
+Receive-Job -Id $j1.id,$j2.id,$j3.id
+
+Get-Job|Remove-Job
