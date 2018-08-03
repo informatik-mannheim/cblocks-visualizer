8db0d1abc949f16935a430be3242e6f192a1240d
Dominick Madden <dominick.madden@gmail.com>
2018-08-03 15:26:27 +0200

    Delete htmlIds (no more need) and mappingDialog (now going to be modal)

--- frontend/src/action/index.js
+++ frontend/src/action/index.js
@@ -1,11 +1,6 @@
-import Constants from '../constants/';
-
 export * from './sensors';
 export * from './connections';
-export * from './mappingDialog';
 export * from './mappings';
 export * from './requests';
 export * from './modals';
 export * from './pinnedCharts';
-
-export const addHtmlIdMapping = (_id, htmlId) => ({type: Constants.Actions.ADD_HTMLIDMAPPING, _id, htmlId});
