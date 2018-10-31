f02287781389c6ee76f38975c48f878efff6aaed
Dominick Madden <dominick.madden@gmail.com>
2018-10-31 16:46:17 +0100

    Fix sliders

--- frontend/src/components/MaterialSlider.js
+++ frontend/src/components/MaterialSlider.js
@@ -1,67 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Nouislider from 'react-nouislider';
 import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/lab/Slider';
+import { withStyles } from '@material-ui/core/styles';
 
-class MaterialSlider0 extends React.Component {
-  static propTypes = {
-    color: PropTypes.string,
-    currentValue: PropTypes.number,
-    instanceID: PropTypes.number,
-    isWriteable: PropTypes.bool,
-    label: PropTypes.string,
-    maximum: PropTypes.number,
-    minimum: PropTypes.number,
-    objectID: PropTypes.number,
-    requestChangeToSubresource: PropTypes.func,
-    resourceID: PropTypes.number
+const styles = {
+  root: {
+    width: 300
+  },
+  slider: {
+    padding: '22px 0px'
   }
+};
+
+class MaterialSlider extends React.Component {
+
 
   constructor () {
     super();
     this.isBeingDragged = false;
   }
 
+  state = {
+    value: 50
+  }
+
+  componentDidMount () {
+    this.value = this.props.currentValue;
+  }
+
   handleDragStart = () => {
     this.isBeingDragged = true;
   };
 
-  handleDragEnd = () => {
-    this.isBeingDragged = false;
+  handleDragEnd = (event) => {
+    this.props.requestChangeToSubresource(this.props.label, Number.parseInt(this.state.value));
+    setTimeout(() => {
+      this.isBeingDragged = false;
+    }, 1000);
   };
 
-  handleOnChange = () => {
-    this.isBeingDragged = false;
-    if (this.refs.NouisliderRef !== undefined) {
-      if (this.props.isWriteable === true) {
-        this.props.requestChangeToSubresource(this.props.label, Number.parseInt(this.refs.NouisliderRef.slider.get()));
+  handleOnChange = (event, value) => {
+      if (this.isBeingDragged === true) {
+          this.setState({value: value});
       } else {
-        this.refs.NouisliderRef.slider.set(this.props.currentValue);
+        this.props.requestChangeToSubresource(this.props.label, Number.parseInt(value));
       }
-    }
-  };
+  }
 
   render () {
-    const slider = (
-      <Nouislider
-        start={[this.props.currentValue]}
-        connect={[true, false]}
-        step={1}
-        range={{ min: this.props.minimum, max: this.props.maximum }}
-        onChange={() => this.handleOnChange()}
-        onStart={() => this.handleDragStart()}
-        onEnd={() => this.handleDragEnd()}
-        ref={'NouisliderRef'}
-      />
-    );
+    const { classes } = this.props;
+    const sliderValue = (this.isBeingDragged === false) ? this.props.currentValue : this.state.value;
 
+    const slider = (
+      <div className={classes.root}>
+        <Slider
+          value={sliderValue}
+          onChange={(event, value) => this.handleOnChange(event, value)}
+          onDragStart={() => this.handleDragStart()}
+          onDragEnd={() => this.handleDragEnd()}
+          min={this.props.minimum}
+          max={this.props.maximum}
+        />
+      </div>
+    );
+//<Typography id="label">{this.props.label !== undefined ? this.props.label.charAt(0).toUpperCase() + this.props.label.slice(1) : ''}</Typography>
     return (
       <div>
-        <Typography id="label">{this.props.label !== undefined ? this.props.label.charAt(0).toUpperCase() + this.props.label.slice(1) : ''}</Typography>
         {slider}
       </div>
     );
   }
 }
 
-export default MaterialSlider0;
+MaterialSlider.propTypes = {
+  classes: PropTypes.object.isRequired,
+  color: PropTypes.string,
+  currentValue: PropTypes.number,
+  instanceID: PropTypes.number,
+  isWriteable: PropTypes.bool,
+  label: PropTypes.string,
+  maximum: PropTypes.number,
+  minimum: PropTypes.number,
+  objectID: PropTypes.number,
+  requestChangeToSubresource: PropTypes.func,
+  resourceID: PropTypes.number
+};
+
+export default withStyles(styles)(MaterialSlider);
